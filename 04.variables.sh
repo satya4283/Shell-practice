204601a9529ca8bcfde06a8c09cf7fab877517a0
Satyanarayana Tamarana <satyanarayana.t@changepond.com>
2026-03-17 11:28:35 +0530

    dynamic arguments

---
+++ 04.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1 # There should be no speace before or after =
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, How are you"
+echo "$PERSON2:: Hello $PERSON1, I am fine, How are you doing"
+echo "$PERSON1:: Doing good, what about you"
+echo "$PERSON2::I am going to mars, are you coming"
+echo "$PERSON1:: Sorry,you caryy on.!! I will come once you are back."
+echo "$PERSON2:: okay, will meet once i retrun from mars."
