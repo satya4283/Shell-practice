f1786cba9c55a6a8e14c5d68ce46754658edbe2e
Satyanarayana Tamarana <satyanarayana.t@changepond.com>
2026-03-17 11:10:21 +0530

    conversation started

---
+++ 03.variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+PERSON1=trump
+PERSON2=musk
+
+echo "$PERSON1: Hi $PERSON2, How are you"
+echo "$PERSON2: Hello $PERSON1, I am fine, How are you doing"
+echo "$PERSON1: Doing good, what about you"
+echo "$PERSON2: I am going to mars, are you coming"
+echo "$PERSON1: No , I will come once you are back."
+ehco "$PERDON2: okay, will meet once i retrun from mars."
