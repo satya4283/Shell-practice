faf42905d8feed5aa2a7116d7eb29a98abd9526e
Satyanarayana Tamarana <satyanarayana.t@changepond.com>
2026-03-17 11:17:44 +0530

    conversation started

--- 03.variables.sh
+++ 03.variables.sh
@@ -3,9 +3,9 @@
 PERSON1=modi
 PERSON2=musk
 
-echo "$PERSON1: Hi $PERSON2, How are you"
-echo "$PERSON2: Hello $PERSON1, I am fine, How are you doing"
-echo "$PERSON1: Doing good, what about you"
-echo "$PERSON2: I am going to mars, are you coming"
-echo "$PERSON1: Sorry,you caryy on.!! I will come once you are back."
-ehco "$PERDON2: okay, will meet once i retrun from mars."
+echo "$PERSON1:: Hi $PERSON2, How are you"
+echo "$PERSON2:: Hello $PERSON1, I am fine, How are you doing"
+echo "$PERSON1:: Doing good, what about you"
+echo "$PERSON2::I am going to mars, are you coming"
+echo "$PERSON1:: Sorry,you caryy on.!! I will come once you are back."
+echo "$PERDON2:: okay, will meet once i retrun from mars."
