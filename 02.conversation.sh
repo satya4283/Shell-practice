d3232bff49fc2221ca0c8a3d24f75e053d85d884
Satyanarayana Tamarana <satyanarayana.t@changepond.com>
2026-03-17 10:55:24 +0530

    conversation started

---
+++ 02.conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hi musk, How are you"
+echo "Musk: Hello trump, I am fine, How are you doing"
+echo "Trump: Doing good, what about you"
+echo "Musk: I am going to mars, are you coming"
+echo "Trump: No , I will come once you are back."
