03b3b06bcafbfc78eed918a1482a307c3d0da27a
Satyanarayana Tamarana <satyanarayana.t@changepond.com>
2026-03-17 11:55:00 +0530

    data-types

---
+++ 06.data-types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is comment
+NUMBER1=100
+NUMBER2=200
+
+TIMESTAMP=$(date)
+
+echo "script executed at: $TIMESTAMP"
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "sum of $NUMBER1 and $NUMBER2 is $SUM"
