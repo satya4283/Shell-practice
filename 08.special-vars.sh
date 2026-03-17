800cf5db20455b38746b414594ff5a3b72349774
Satyanarayana Tamarana <satyanarayana.t@changepond.com>
2026-03-17 18:53:45 +0530

    special vars

--- 08.special-vars.sh
+++ 08.special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of variables: $#"
+echo "script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home Directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in the background: $!"
